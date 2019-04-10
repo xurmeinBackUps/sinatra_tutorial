7a3a727a4ea71a8d6175bf7cb90557123266807b
xurmeinBackUps <wj.pavocrist@gmail.com>
2019-04-10 14:01:42 -0500

    getting close

--- sinatra_app/web_guesser.rb
+++ sinatra_app/web_guesser.rb
@@ -3,6 +3,25 @@ require 'sinatra/reloader'
 
 number = rand(100)
 
+results = ['Way too high!', 'Too high!', 'Too low!', 'Way too low!', 'You guessed the right number!']
+
+def check_guess(guess)
+    if guess == number 
+        puts results.last
+    elseif guess >= number + 5
+        puts results[0]
+    elseif guess == number + 1 ... number + 5
+        puts results[1]
+    elseif guess == number - 1 ... number - 5
+        puts results[2]
+    else guess <= number - 5 
+        puts results[4]
+    end
+end
+
+
 get '/' do
-    erb :index, :locals => {:number => number}
+    guess = params['guess'.to_i]
+    results = check_guess('guess')
+    erb :index, :locals => {:number => number, :results => results}
 end
